b68a80b7c2c5de75bb1e6aa224353f9dbc4ca5ef
Adam Taitano <adam.taitano@gmail.com>
2015-09-28 16:17:27 -0700

    Make data persistent using MongoDB

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-var config = require('./config.js');
 var express = require("express");
 var bodyParser = require("body-parser");
-var db = require('orchestrate')(config.dbkey);
+var mongojs = require("mongojs");
+var db = mongojs("todo", ["users", "tasks"]);
 
 var app = express();
 
@@ -9,131 +9,59 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(__dirname));
 
-// var users = [];
-// var users = ["Sparky", "Skippy", "Arturo"];
-// var tasks = [];
-// var tasks = [
-//   ["Steal Redvines","Run from coppers","Skippy","","Unassigned"],
-//   ["The big bug","don't break","Sparky","","Unassigned"],
-//   ["broken one","this one is broken","Skippy","Skippy","Done"],
-//   ["Something","Something else","Anyone","Skippy","Assigned"],
-//   ["Give Redvines","Hi five coppers","Evil Abe","","Unassigned"],
-//   ["Write a book","finish it before i die(GRRM)","Kanye East","Arturo","Assigned"]
-// ];
-
 app.get('/users', function (req, res) {
-  db.list('users')
-  .then(function (result) {
-    var data = result.body.results;
-    var mapped = data.map(function (element, index) {
-      return {id: element.path.key, username: element.value.username};
+  db.users.find(function(error, documents) {
+    var mapped = documents.map(function(element) {
+      return {id: element._id, username: element.username};
     });
     console.log(mapped);
+    if(error !== null) {console.log("Error occurred; error message is: ", error);}
     res.send(mapped);
   });
 });
-
-  // .fail(function (err) {  console.log("this failed :", err);
-
-  // var usersAndIDs = users.map(function (element, index) {
-  //   return {id: index, username: element};
-  // });
-  // console.log(mapped);
 
 app.get('/tasks', function (req, res) {
-  db.list('tasks')
-  .then(function (result) {
-    var data = result.body.results;
-    var mapped = data.map(function (element, index) {
-      return {id: element.path.key, title: element.value.title, description: element.value.description, creator: element.value.creator, assignee: element.value.assignee, status: element.value.status};
+  db.tasks.find(function(error, documents) {
+    var mapped = documents.map(function(element) {
+      return {id: element._id, title: element.title, description: element.description, creator: element.creator, assignee: element.assignee, status: element.status};
     });
     console.log(mapped);
+    if(error !== null) {console.log("Error occurred; error message is: ", error);}
     res.send(mapped);
   });
-  // var tasksAndIDs = tasks.map(function (element, index) {
-  //   return {id: index, title: element[0], description: element[1], creator: element[2], assignee: element[3], status: element[4]};
-  // });
-  // res.send(tasksAndIDs);
 });
 
-// app.put('/users/:id', function (req, res) {
-//     var id = req.params.id;
-//     users[id] = req.body.username;
-//     res.send({id: id});
-// });
-
 app.post('/users', function (req, res) {
-    // create key of new user
-    var id;
-    db.list('users')
-    .then(function(result) {
-      id = result.body.count + 1;
-      console.log(id);
-      db.put('users', id, {"username" : req.body.username})
-        .then(function(result) {
-          console.log(req.body.username);
-          res.send({id: id});
+    db.users.find(function(error, documents) {
+        db.users.insert(req.body, function(err, docs) {
+        console.log(docs);
+        res.send({id: docs._id});
       });
     });
-    // var id = users.length;
-    // users[id] = req.body.username;
 });
 
 app.put('/tasks/:id', function (req, res) {
     var id = req.params.id;
-    db.put('tasks', id, {"title" : req.body.title, "description" : req.body.description, "creator" : req.body.creator, "assignee" : req.body.assignee, "status": req.body.status})
-      .then(function(result) {
-        console.log("tasks update works");
+    db.tasks.findAndModify(
+      {
+        query: {_id: mongojs.ObjectId(id)},
+        update: {$set: {"title" : req.body.title, "description" : req.body.description, "creator" : req.body.creator, "assignee" : req.body.assignee, "status": req.body.status}},
+        new: true
+      },
+      function(error, document) {
+        console.log("Updated document contains: ", document);
         res.send({id: id});
-    });
-    // tasks[id] = [req.body.title, req.body.description, req.body.creator, req.body.assignee, req.body.status];
-    // res.send({id: id});
+      }
+    );
 });
 
 app.post('/tasks', function (req, res) {
-    // var id = tasks.length;
     var id;
-    db.list('tasks')
-    .then(function(result) {
-      id = result.body.count + 1;
-      // console.log(id);
-      db.put('tasks', id, {"title" : req.body.title, "description" : req.body.description, "creator" : req.body.creator, "assignee" : req.body.assignee, "status": req.body.status})
-        .then(function(result) {
-          console.log("tasks create works");
-          res.send({id: id});
-      });
+    db.tasks.insert({"title" : req.body.title, "description" : req.body.description, "creator" : req.body.creator, "assignee" : req.body.assignee, "status": req.body.status}, function(error, documents) {
+      console.log("New document added contains: ", documents);
+      res.send({id: documents._id});
     });
-    // tasks[id] = [req.body.title, req.body.description, req.body.creator, req.body.assignee, req.body.status];
-    // res.send({id: id});
 });
-
-
-
-// app.get('/texts/:id', function (req, res) {
-//     var id = req.params.id;
-//     res.send(JSON.stringify({id: id, value : texts[id]}));
-// });
-//
-// app.put('/texts/:id', function (req, res) {
-//     var id = req.params.id;
-//     tasks[id] = req.body.value;
-//     res.send({id: id});
-// });
-//
-// app.delete('/texts/:id', function (req, res) {
-//     var id = req.params.id;
-//     texts.splice(id, 1);
-//     res.send({id: id});
-// });
-//
-// app.get('/texts', function (req, res) {
-//     var textsAndIDs = texts.map(function (v, i) {
-//         return {id : i, value : v};
-//     });
-//     res.send(textsAndIDs);
-// });
-
-
 
 app.listen(3000, function () {
   console.log("Server is running...........");
